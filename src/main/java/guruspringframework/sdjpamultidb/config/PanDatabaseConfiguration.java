879466ad22df7af19934ed3238053612b895137f
Stefan Georgesco <stefan.georgesco@yahoo.fr>
2023-05-06 16:48:22 +0200

    Set Hikari pool names

--- src/main/java/guruspringframework/sdjpamultidb/config/PanDatabaseConfiguration.java
+++ src/main/java/guruspringframework/sdjpamultidb/config/PanDatabaseConfiguration.java
@@ -34,6 +34,7 @@ public class PanDatabaseConfiguration {
 
     @Bean
     @Primary
+    @ConfigurationProperties("spring.pan.datasource.hikari")
     public DataSource panDataSource(@Qualifier("panDataSourceProperties")
                                      DataSourceProperties panDataSourceProperties) {
         return panDataSourceProperties.initializeDataSourceBuilder()
